b6cce739d4d03af658f522701914c10a7f378854
berkanuz <berkanuz@hotmail.com>
2021-01-28 12:24:03 +0300

    Initial commit

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+    
+        $user = User::where('email', $request->email)->first();
+    
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            return response([
+                'message' => ['The provided credentials are incorrect.']
+            ], 500);
+        }
+
+        $userToken = $user->createToken('api-token')->plainTextToken;
+    
+        return response(['token' => $userToken], 200);
+    }
